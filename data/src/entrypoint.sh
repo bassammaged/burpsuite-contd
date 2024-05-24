eaa55ecfaeebabdfe9454be770bc4a43affa1f0c
kemet <kemet@Anubis.secranix.local>
2024-05-24 11:07:17 -0400

    Fix curl issue

--- data/src/entrypoint.sh
+++ data/src/entrypoint.sh
@@ -1,7 +1,7 @@
 # Download the selected burpsuite version
-if [ ! -f "/home/burp/src/burpsuite_pro.jar" ]; then
+if [ ! -f "/home/burp/src/burpsuite_community.jar" ]; then
     echo "[+] Downloading Burpsuite community..."
-    curl "https://portswigger.net/burp/releases/download?product=community&version=2024.4.4&type=Jar" -O /home/burp/src/burpsuite_community.jar
+    curl -o /home/burp/src/burpsuite_community.jar "https://portswigger.net/burp/releases/download?product=community&version=2024.4.4&type=Jar"
 fi
 
 java -jar /home/burp/src/burpsuite_community.jar
